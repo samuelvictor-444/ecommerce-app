2fe84d93b31e4ace455d3e2a3333bee60a5c8001
samuelvictor-444 <samtexvictor9@gmail.com>
2025-08-26 15:32:13 +0100

    stil working on it

--- assets/javascript/displayCartItems.js
+++ assets/javascript/displayCartItems.js
@@ -1,5 +1,38 @@
-window.addEventListener("DOMContentLoaded" , () => {
-   const cart = JSON.parse(localStorage.getItem("cart"));
+window.addEventListener("DOMContentLoaded" , async () => {
+   const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  
+   const emptyCartContainer = document.querySelector(".empty_cart_box");
 
-   
+   if(!emptyCartContainer) return;
+
+   // check if cart is empty
+   if(cart.length === 0) {
+     emptyCartContainer.style.display = "block";
+     return;
+   }
+
+   try {
+
+      const response = await fetch("api/getCartDetails.php", {
+         method:"POST",
+         headers:{"Content-Type": "application/json"},
+         body: JSON.stringify(cart)
+      });
+
+      if(response.ok) {
+           const products = await response.json();
+
+           if(products) {
+            
+           }
+
+      }else {
+         throw new Error(`HTTPS ERROR STATUS ${response.status}`);
+      }
+      
+   } catch (error) {
+      console.error("error fetching cart products", error);
+   }
+
+
 });
