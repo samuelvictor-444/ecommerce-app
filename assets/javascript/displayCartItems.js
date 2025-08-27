b2bc5942744e99c576d55027d1844eb36eab0517
samuelvictor-444 <samtexvictor9@gmail.com>
2025-08-28 00:09:46 +0100

    stil working on it

--- assets/javascript/displayCartItems.js
+++ assets/javascript/displayCartItems.js
@@ -60,21 +60,21 @@ window.addEventListener("DOMContentLoaded", async () => {
           const discountPercent =
             oldPrice > 0 ? Math.round((discountAmount / oldPrice) * 100) : 0;
 
-          const productVariation = product.variation
-            ? `${product.variation}`
-            : "";
-          const productVariationName = product.variationName
-            ? `${product.variationName}`
-            : "";
+           
+            
 
           let variationHTML = "";
           if (product.variation && product.variationName) {
             variationHTML = `<p id="varya"> <span id="vN"> ${product.variationName}:<span id="v_V"> ${product.variation}</p>`;
           }
 
+          $valueId = product.attrId;
+
           wrapper.innerHTML += `
           
-          <article class="_product_added dr" id="product_c">
+          <article class="_product_added dr" id="product_c" data-value="${
+            $valueId ? `${$valueId}` : ""
+          }"  data-slug="${product.slug}">
                         <a href="product.php?id=${product.id}&name=${
             product.slug
           }" id="core">
@@ -109,19 +109,21 @@ window.addEventListener("DOMContentLoaded", async () => {
                             </div>
                         </a>
                         <section class="bt">
-                            <button class="btns_">
+                            <button class="btns_ removeItem">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M600-240v-80h160v80H600Zm0-320v-80h280v80H600Zm0 160v-80h240v80H600ZM120-640H80v-80h160v-60h160v60h160v80h-40v360q0 33-23.5 56.5T440-200H200q-33 0-56.5-23.5T120-280v-360Zm80 0v360h240v-360H200Zm0 0v360-360Z"/></svg>
                             Remove</button>
 
                             <div class="-mal">
-                                <button id="" class="_qty inactive" value="" type="button" disabled="">
+                                <button id="decrement" class="_qty decrement" value="" type="button"   ${
+                                  product.quantity > 1 ? "" : "disabled"
+                                }>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                         style="fill: rgba(0, 0, 0, 1);">
                                         <path d="M5 11h14v2H5z"></path>
                                     </svg>
                                 </button>
                                 <span class="incre">${product.quantity}</span>
-                                <button id="" class="_qty" type="button">
+                                <button id="increment" class="_qty increment" type="button">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                         style="fill: rgba(0, 0, 0, 1);">
                                         <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>
@@ -134,6 +136,8 @@ window.addEventListener("DOMContentLoaded", async () => {
           totalP.forEach((item) => {
             item.innerHTML = `&#x20A6;` + " " + formatPrice(total);
           });
+
+          document.dispatchEvent(new Event("cart"));
         });
       }
     } else {
